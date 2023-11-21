64470644de61fcbff1824499eb7f4a311b138b41
Mason <caohaison123@gmail.com>
2023-11-22 08:46:19 +1100

    Initial Commit

---
+++ MasonDegrees/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MasonDegrees
+//
+//  Created by Hai Son Cao on 22/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MasonDegrees/MasonDegreesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasonDegreesApp.swift
+//  MasonDegrees
+//
+//  Created by Hai Son Cao on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MasonDegreesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
