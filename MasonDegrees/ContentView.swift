bbb2e275e9c092f43873b50b416c55cd4602c394
Mason <caohaison123@gmail.com>
2023-11-22 17:09:09 +1100

    Implemented enum cases in place of strings

--- MasonDegrees/ContentView.swift
+++ MasonDegrees/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    let unitOptions = ["Celcius", "Fahrenheit", "Kelvin"]
+    
+    enum TemperatureUnit: String, CaseIterable {
+        case Celcius
+        case Fahrenheit
+        case Kelvin
+        var type: String {self.rawValue}
+    }
     
     @FocusState var isInputFocused: Bool
-    @State private var inputUnit = "Celcius"
+    @State private var inputUnit = TemperatureUnit.Celcius
     @State private var inputDegrees = 0.0
-    @State private var outputUnit = "Fahrenheit"
+    @State private var outputUnit = TemperatureUnit.Fahrenheit
+
+//    Convert all input units to celcius
+    var toCelcius : Double {
+        switch inputUnit {
+        case .Celcius:
+            return inputDegrees
+        case .Fahrenheit:
+            return (inputDegrees - 32) * (5/9)
+        case .Kelvin:
+            return inputDegrees - 273.15
+        }
+    }
     
     private var outputCelcius: Double {
         toCelcius
@@ -24,36 +42,30 @@ struct ContentView: View {
     private var outputKelvin: Double {
         toCelcius + 273.15
     }
+    
+    func displayOutputTemp(for outputUnit: TemperatureUnit) -> Text {
+        switch outputUnit {
+        case .Celcius:
+            return Text("\(outputCelcius.formatted())°C")
+        case .Fahrenheit:
+            return Text("\(outputFahrenheit.formatted())°F")
+        case .Kelvin:
+            return Text("\(outputKelvin.formatted())°K")
 
-//    Convert all input units to celcius
-    var toCelcius : Double {
-        if inputUnit == "Fahrenheit" {
-            return (inputDegrees - 32) * (5/9)
-        } else if inputUnit == "Kelvin" {
-            return inputDegrees - 273.15
         }
-        return inputDegrees
+        
     }
     
-    func displayOutputTemp(for outputUnit: String) -> Text {
-        if outputUnit == "Celcius" {
-            Text("\(outputCelcius.formatted())°C")
-        } else if outputUnit == "Fahrenheit" {
-            Text("\(outputFahrenheit.formatted())°F")
-        } else {
-            Text("\(outputKelvin.formatted())°K")
-        }
-    }
     
-
     var body: some View {
         NavigationStack {
             Form {
 //                Input temperature conversion
                 Section("Input") {
-                    Picker("Input degree", selection: $inputUnit) {
-                        ForEach(unitOptions, id: \.self) {
-                            Text($0)
+                    Picker("Input unit", selection: $inputUnit) {
+                        ForEach(TemperatureUnit.allCases, id: \.rawValue) {
+                            Text($0.type)
+                                .tag($0)
                         }
                     }
                     .pickerStyle(.segmented)
@@ -61,20 +73,20 @@ struct ContentView: View {
                     TextField ("0.0", value: $inputDegrees, format: .number)
                         .keyboardType(.numberPad)
                         .focused($isInputFocused)
+               
                 }
-                
                 
 //                Output temperature conversion
                 Section("Output") {
                     Picker("0.0", selection: $outputUnit) {
-                        ForEach(unitOptions, id: \.self) {
-                            Text($0)
+                        ForEach(TemperatureUnit.allCases, id: \.rawValue) {
+                            Text($0.type)
+                                .tag($0)
                         }
                     }
                     .pickerStyle(.segmented)
                     
-                    
-                   displayOutputTemp(for: outputUnit)
+                    displayOutputTemp(for: outputUnit)
                 }
                 
             }
